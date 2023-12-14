4a64864b2d2d5271d3c11a13082487eba7599142
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-14 09:02:26 +0100

    solve challenge

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,29 @@ import GlobalStyle from "../styles";
 import styled from "styled-components";
 
 const StyledNav = styled.nav`
-  border-bottom: solid 2px grey;
+  background-color: aliceblue;
+  &:hover {
+    border-bottom: solid 2px grey;
+  }
   padding: 1rem;
 `;
 
 const StyledList = styled.ul`
-  list-style: none;
+  list-style-type: none;
   display: flex;
   gap: 1rem;
+`;
+
+const StyledLink = styled.a`
+  padding: 5px 10px;
+  border-radius: 3px;
+  border: solid 1px lightgray;
+  text-decoration: none;
+  color: black;
+  &:hover {
+    background-color: lightblue;
+    border: solid 1px lightblue;
+  }
 `;
 
 export default function App({ Component, pageProps }) {
@@ -20,13 +35,13 @@ export default function App({ Component, pageProps }) {
       <StyledNav>
         <StyledList>
           <li>
-            <Link href="/">Home</Link>
+            <StyledLink href="/">Home</StyledLink>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <StyledLink href="/about">About</StyledLink>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <StyledLink href="/contact">Contact</StyledLink>
           </li>
         </StyledList>
       </StyledNav>
